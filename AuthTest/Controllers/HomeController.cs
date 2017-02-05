69c81b4622ac025ac851746a5c2d917bb68d73b4
shenqiangbin <shenqiangbin@163.com>
2017-02-05 12:29:48 +0800

    Add project files.

---
+++ AuthTest/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using AuthTest.Filters;
+
+namespace AuthTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        public ActionResult SignIn(string returnUrl)
+        {
+            //todo:添加登录验证代码
+            FormsAuthentication.SetAuthCookie("admin", false);
+            if (string.IsNullOrEmpty(returnUrl))
+                return RedirectToAction("Index");
+            else
+                return Redirect(returnUrl);
+        }
+
+        public ActionResult SignOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult unAuthrize()
+        {
+            return Content("无权限访问");
+        }
+
+        //新闻
+        [UserAuthorize]
+        public ActionResult News()
+        {
+            return View();
+        }
+
+        //关于
+        [UserAuthorize]
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ AuthTest/Filters/UserAuthorize.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuthTest.Filters
+{
+    public class UserAuthorizeAttribute : AuthorizeAttribute
+    {
+        private bool isAuthorize;
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            base.HandleUnauthorizedRequest(filterContext);
+            if (isAuthorize)//如果认证了，只是没有权限则展示无权限界面
+                filterContext.Result = new RedirectResult("/home/unAuthrize");
+        }
+
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            base.OnAuthorization(filterContext);
+        }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            isAuthorize = base.AuthorizeCore(httpContext);
+            bool canAccess = IsInRole(httpContext.User.Identity.Name, httpContext.Request.Url.AbsolutePath);
+            return isAuthorize && canAccess;
+        }
+
+        protected override HttpValidationStatus OnCacheAuthorization(HttpContextBase httpContext)
+        {
+            return base.OnCacheAuthorization(httpContext);
+        }
+
+        private bool IsInRole(string account, string url)
+        {
+            //todo:添加角色验证代码
+            if (account == "admin" && url == "/home/about")
+                return false;
+            else
+                return true;
+        }
+    }
+}
